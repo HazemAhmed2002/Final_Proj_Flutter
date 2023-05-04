e218d2575e9f5d761befdb977a58a2430a8c11f6
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-05-04 17:50:05 +0300

    hazem

--- lib/screens/login screens/register_screen.dart
+++ lib/screens/login screens/register_screen.dart
@@ -4,28 +4,34 @@ import 'package:todaydo_app/screens/tasks_screen.dart';
 
 import '../../widgets/textFieldWidget.dart';
 
-
 class RegisterScreen extends StatelessWidget {
-   RegisterScreen({Key? key}) : super(key: key);
+  RegisterScreen({Key? key}) : super(key: key);
+
   late TextEditingController _textEmailEditingController;
   late TextEditingController _textPasswordEditingController;
-   late TextEditingController _textNameEditingController;
+  late TextEditingController _textNameEditingController;
+
   String? _emailError;
   String? _passwordError;
 
   @override
   void initState() {
     _textEmailEditingController = TextEditingController();
+
     _textPasswordEditingController = TextEditingController();
-      _textNameEditingController = TextEditingController();
+
+    _textNameEditingController = TextEditingController();
   }
 
   @override
   void dispose() {
     _textEmailEditingController.dispose();
+
     _textPasswordEditingController.dispose();
-       _textNameEditingController.dispose();
+
+    _textNameEditingController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,32 +52,57 @@ class RegisterScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: const Color(0XFF23203F)),
               ),
-              SizedBox(height: 9,),
+              SizedBox(
+                height: 9,
+              ),
               Text(
-                'قم بإنشاء حساب لبدء استخدام التطبيق', style: GoogleFonts.roboto(
-                  fontSize: 18, color: const Color(0XFF716F87),),),
-              const SizedBox(height: 36,),
-              TextFieldWidget(hint: 'الاسم', tet: TextInputType.name,textEditingController: _textNameEditingController, ),
-              const SizedBox(height: 30,),
-              TextFieldWidget(hint: 'البريد الالكتروني', tet: TextInputType.emailAddress,textEditingController: _textEmailEditingController),
-              const SizedBox(height: 30,),
-              TextFieldWidget(hint: 'كلمة المرور', tet: TextInputType.visiblePassword,textEditingController: _textPasswordEditingController),
-              const SizedBox(height: 30,),
-              Center(
-                child: TextButton(onPressed: (){
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => TasksScreen(),
-                  ));
-                }, child: Text('تسجيل') , style: TextButton.styleFrom(
-                    backgroundColor: Colors.limeAccent,
-                    primary: Colors.red
-                ),
+                'قم بإنشاء حساب لبدء استخدام التطبيق',
+                style: GoogleFonts.roboto(
+                  fontSize: 18,
+                  color: const Color(0XFF716F87),
                 ),
               ),
-
+              const SizedBox(
+                height: 36,
+              ),
+              TextFieldWidget(
+                hint: 'الاسم',
+                tet: TextInputType.name,
+                textEditingController: _textNameEditingController,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFieldWidget(
+                  hint: 'البريد الالكتروني',
+                  tet: TextInputType.emailAddress,
+                  textEditingController: _textEmailEditingController),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFieldWidget(
+                  hint: 'كلمة المرور',
+                  tet: TextInputType.visiblePassword,
+                  textEditingController: _textPasswordEditingController),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => TasksScreen(),
+                    ));
+                  },
+                  child: Text('تسجيل'),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.limeAccent, primary: Colors.red),
+                ),
+              ),
             ],
           ),
         ),
       ),
-    );  }
+    );
+  }
 }
