f39cad7ebb875ad20f8edba04ce2cb8393521b15
Hesham Rajab <hesham.7.r@gmail.com>
2023-05-05 04:56:06 +0300

    hesham

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todaydo_app/models/task_data.dart';
-import 'package:todaydo_app/screens/login_screen.dart';
-import 'package:todaydo_app/screens/tasks_screen.dart';
+import 'package:todaydo_app/screens/login%20screens/login_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
