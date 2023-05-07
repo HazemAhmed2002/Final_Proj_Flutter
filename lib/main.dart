f19126cfc373f238125da38da12d3aee7f141461
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-05-07 20:02:46 +0300

    hazem

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:todaydo_app/models/task_data.dart';
 import 'package:provider/provider.dart';
 import 'package:todaydo_app/screens/tasks_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 
 
 void main() async{
